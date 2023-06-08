da97b9209a10f016ec8d805a6f1e45a9b73e5c19
salim imuzai <salimimuzai@gmail.com>
2023-06-08 16:19:25 +0100

    add default user to user table

--- src/V1/APIs/Config/migrations/20221129074643_column-users.ts
+++ src/V1/APIs/Config/migrations/20221129074643_column-users.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.alterTable('users', (table) => {
-        table.string('user_type', 50).notNullable();
+        table.string('user_type', 50).notNullable().defaultTo('User');
         table.boolean('is_active').notNullable().defaultTo('True');
         table
             .specificType('long_lat', 'POINT')
